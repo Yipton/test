7d162951f5272aee6474a3396ea83f173fd9e870
Yipton <niniss.yp@gmail.com>
2024-07-07 17:51:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Exemple de fonction JavaScript pour interactivité
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Le DOM est prêt.');
+    // Vous pouvez ajouter d'autres scripts JavaScript ici
+});
